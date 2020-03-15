0bf9b259e25d0d56d22ce7fb71eaa31e18e07123
Christopher Sannar <chris.sannar.dev@gmail.com>
2020-03-14 20:25:45 -0600

    Some minor improvements...

--- main.go
+++ main.go
@@ -14,41 +14,40 @@ type RPSLog struct {
 	ID string `json:"id"`
 	// Time string `json:"timestamp"`
 	Winner string `json:"winner"`
+	Looser string `json:"looser"`
 	Combo  string `json:"combo"`
 }
 
-type RPSLogs []RPSLog
+var logs []RPSLog
 
 func homePage(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Welcome to RPS!")
-	// Input their name, and check a hashtable if someone else has that name already
+	http.ServeFile(w, r, "./html/index.html")
 }
 
 func getGamePage(w http.ResponseWriter, r *http.Request) {
-	logs := RPSLogs{
-		RPSLog{ID: "1", Winner: "Duders", Combo: "R P"},
-	}
+	logs = append(logs, RPSLog{ID: "1", Winner: "Dude", Looser: "Guy", Combo: "PR"})
+	fmt.Fprintf(w, "Game page")
+}
 
-	// fmt.Fprintf(w, "Game page")
+func apiGETHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(logs)
 }
 
-func postGamePage(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "POST working")
-}
-
-func apiHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "API")
+func apiPOSTHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(logs)
+	fmt.Println("POST")
 }
 
 func handleRequests() {
 
 	router := mux.NewRouter().StrictSlash(true)
 
-	router.HandleFunc("/", homePage).Methods("GET")
+	router.HandleFunc("/", homePage)
 	router.HandleFunc("/game", getGamePage).Methods("GET")
-	router.HandleFunc("/game", postGamePage).Methods("POST")
-	router.HandleFunc("/api", apiHandler).Methods("GET")
+	router.HandleFunc("/api/get", apiGETHandler).Methods("GET")
+	router.HandleFunc("/api/post", apiGETHandler).Methods("POST")
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
 
